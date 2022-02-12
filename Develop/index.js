68282ad0af8b8a7106135549ba8c239b56306149
ajmarrocco <ajmarrocco@gmail.com>
2022-02-12 14:57:20 -0500

    title prompt

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,33 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'name',
+        message: 'What is your project name? (Required)',
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+                console.log('Please enter your project namedfg!');
+                return false;
+            }       
+        }
+    }
+];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+const promptUser = () => {
+    return inquirer.prompt(questions);
+}
 
-// TODO: Create a function to initialize app
-function init() {}
+// // TODO: Create a function to write README file
+// function writeToFile(fileName, data) {}
 
-// Function call to initialize app
-init();
+// // TODO: Create a function to initialize app
+// function init() {}
+
+// // Function call to initialize app
+// init();
+
+promptUser();
